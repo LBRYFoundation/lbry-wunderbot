138153a706c55842da66760e16ae5c6ac6aa1cda
Alex Grintsvayg <alex@grin.io>
2016-07-18 10:17:39 -0400

    more explicit notice

--- tipbot.js
+++ tipbot.js
@@ -150,8 +150,8 @@ function doHelp(bot, channel) {
     '`' + command + ' withdraw ADDRESS AMOUNT`: withdraw AMOUNT credits to ADDRESS\n' +
     '`' + command + ' USER AMOUNT`: send AMOUNT credits to USER\n' +
     '\n' +
-    'Send me a Direct Message if you want to interact privately.\n' +
-    'If I\'m not responding in some channel, you can invite me by @mentioning me\n'
+    '*Everyone will see what I say. Send me a Direct Message if you want to interact privately.*\n' +
+    'If I\'m not responding in some channel, you can invite me by @mentioning me.\n'
   , globalSlackParams);
 }
 
