e458fd5838f12607c3d72686db0361f3b47e2d84
Alex Grintsvayg <alex@grin.io>
2016-08-16 12:39:24 -0400

    refactor

--- app.js
+++ app.js
@@ -12,25 +12,28 @@ var slackbot = new SlackBot({
   name: 'wunderbot'
 });
 
-function sendwelcomemsg(usertowelcome) {
-    request('https://raw.githubusercontent.com/lbryio/lbry.io/master/posts/other/slack-greeting.md', function (error, response, body) {
-       if (!error && response.statusCode == 200) {
-       bot.postMessage(usertowelcome, body);
-      }
-    })};
 
-var tipbot = require('./tipbot');
+
+function sendWelcomeMessage(usertowelcome) {
+  request('https://raw.githubusercontent.com/lbryio/lbry.io/master/posts/other/slack-greeting.md', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      slackbot.postMessage(usertowelcome, body);
+    }
+  });
+};
+
+var tipbot = require('./bots/tipbot');
 tipbot.init(process.env.RPCUSER, process.env.RPCPASSWORD);
 
-var hashbot = require('./hashbot');
+var hashbot = require('./bots/hashbot');
 hashbot.init(slackbot, process.env.MINING_CHANNEL);
 
 
 
 slackbot.on('start', function() {
   slackbot.on('message', function(data) {
-        if (data.type == 'team_join') {
-        setTimeout(function() { sendwelcomemsg(data.user.id); },2000); //Delay because of slow slack api updates which sometimes does not send msg.
+    if (data.type == 'team_join') {
+      setTimeout(function() { sendWelcomeMessage(data.user.id); }, 2000); //Delay because of slow slack api updates which sometimes does not send msg.
     }
     if (data.text) {
       var command = data.text.trim().split(' ')[0];
