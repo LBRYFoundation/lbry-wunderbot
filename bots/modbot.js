2b66ad5921183098d0866d68ebc80c75ec6cf1ef
Fillerino <fillerix@fillerix.se>
2017-06-21 21:14:04 +0200

    Added more moderation stuff: Pins and Locked Channels!
    Added more moderation stuff: Pins and Locked Channels!

--- bots/modbot.js
+++ bots/modbot.js
@@ -65,12 +65,40 @@ function check(slackbot, data) {
       }else if(!usr.is_bot){
         mongo.collection('m_topic').findOne({'channel': data.channel}, function(err, document) {
           slackbot.postMessage(data.user,`Hey <@${data.user_profile.name}>, you are not allowed to change the topic in <#${data.channel}>!`, globalSlackParams);
+          if(process.env.CHANNEL_LOG){
+            slackbot.postMessage(process.env.CHANNEL_LOG,`User <@${data.user_profile.name}> tried to change the topic in <#${data.channel}> to: \`\`\`${data.topic}!\`\`\``, globalSlackParams);
+          }
           needle.get(`https://slack.com/api/channels.setTopic?token=${apitoken}&channel=${data.channel}&topic=${document.topic}`);
         });
       }
     }, err => {
       console.log(err);
     })
+  }
+  if(data.type == 'pin_added'){ // Gets called on a user pin
+  needle.get(`https://slack.com/api/users.info?token=${apitoken}&user=${data.user}`, function(err, resp) {
+    if(!resp.body.user.is_admin){
+      needle.get(`https://slack.com/api/pins.remove?token=${apitoken}&channel=${data.item.channel}&timestamp=${data.item.message.ts}`);
+      slackbot.postMessage(data.user,`Hey <@${resp.body.user.name}>, you are not allowed to add pins in <#${data.channel_id}>!`, globalSlackParams);
+      if(process.env.CHANNEL_LOG){
+          slackbot.postMessage(process.env.CHANNEL_LOG,`User <@${resp.body.user.name}> tried to pin a message in <#${data.channel_id}>!`, globalSlackParams);
+      }
+    }
+  });
+  }
+  if(data.channel == process.env.CHANNEL_OA && data.type == 'message'){ //If user is trying to post in a admin only channel..
+  needle.get(`https://slack.com/api/users.info?token=${apitoken}&user=${data.user}`, function(err, resp) {
+    if(!resp.body.user.is_admin){
+      console.log(`https://slack.com/api/chat.delete?token=${apitoken}&ts=${data.ts}&channel=${data.channel}`);
+      needle.get(`https://slack.com/api/chat.delete?token=${apitoken}&ts=${data.ts}&channel=${data.channel}`);
+      if(data.subtype !=='channel_join' && data.subtype !=='channel_leave') {
+        slackbot.postMessage(data.user,`Hey <@${resp.body.user.name}>, you are not allowed to post messages in <#${data.channel}>!`, globalSlackParams);
+        if(data.subtype !=='channel_join' && data.subtype !=='channel_leave' && process.env.CHANNEL_LOG){
+          slackbot.postMessage(process.env.CHANNEL_LOG,`User <@${resp.body.user.name}> tried to post a message in <#${data.channel}>!`, globalSlackParams);
+        }
+      }
+    }   
+  });
   }
 }
 
