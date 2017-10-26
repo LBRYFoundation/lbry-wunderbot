5a4cf549d12d6cdcfa801961465142420102e008
MSFTserver <sawyerlemmo@gmail.com>
2017-10-25 18:02:56 -0700

    getting speech bot ready

--- bot/modules/speech.js
+++ bot/modules/speech.js
@@ -1,4 +1,4 @@
-/*'use strict';
+'use strict';
 
 exports.commands = [
 	"speech"
@@ -12,7 +12,24 @@ exports.speech = {
 	usage: "<claim>",
 	description: 'Speech coming soon...',
 	process: function(bot,msg,suffix){
-		console.log(msg.member.user);
+		console.log("user " + msg.member.user.username + " used !speech command");
+		if (suffix === "help") {
+		  msg.channel.send(
+		  	   {
+	  "embed": {
+		"title": "Speech bot help",
+		"description": '**!speech `<Name>`** : *displays top claim on speech* \n' +
+    '**!speech `<Name> <URL> <NSFW>`** : *Uploads Image URL to Spee.ch* \n' +
+    '**NOTE : dont include spaces in name (NSFW is optional true/false, if left blank will defualt to false)** \n' +
+	'EXAMPLE : `!speech my-image-name https://url/to/image.png false`',
+		"color": 7976557,
+		"author": {
+		  "name": "COMING SOON!!",
+		  "icon_url": "https://i.imgur.com/yWf5USu.png"
+		}
+	  }
+	}
+	)
+	} else { msg.channel.send("**COMING SOON**"); };
 	}
 }
-*/
