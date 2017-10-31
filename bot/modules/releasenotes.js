f560b998c653a4f9cdcdba6ab698efd57d57a10d
MSFTserver <sawyerlemmo@gmail.com>
2017-10-31 11:21:55 -0700

    update release notes for config

--- bot/modules/releasenotes.js
+++ bot/modules/releasenotes.js
@@ -1,6 +1,7 @@
 var request = require('request');
-let hasPerms = require('../helpers.js').hasPerms;
-
+var config = require('config');
+var hasPerms = require('../helpers.js').hasPerms;
+var ChannelID = config.get('gitrelease').channel;
 
 exports.commands = [
 	"releasenotes" // command that is in this file, every command needs it own export as shown below
@@ -31,10 +32,9 @@ exports.releasenotes = {
 				releaseurl = JSON.parse(body).html_url
 				message = {"embed": {"title": "*Download " + releasename + " here!*","description": releasemessage,"url": releaseurl,"color": 7976557,"timestamp": releasedate,"author": {"name": "Lbry-app Release Notes for " + releasename,"icon_url": "http://www.pngall.com/wp-content/uploads/2016/04/Github-PNG-Image.png"},"footer": {"icon_url": "https://i.imgur.com/yWf5USu.png","text": "Lbry-app Updated "}}}
 				if ( hasPerms(msg) === true && suffix === "post")  {
-					var channelID = "370779899650375681"
-					bot.channels.get(channelID).send(message)
+					bot.channels.get(ChannelID).send(message)
 				} else {
-				msg.channel.send(msg.author + " Release notes sent via DM")
+				msg.channel.send(":small_blue_diamond: Release notes sent via DM")
 				msg.author.send(message)
 				}
 		})
