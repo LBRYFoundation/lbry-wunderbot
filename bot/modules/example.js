28fd9fbfd24d83e4b8f56a0a8cb6df8eea270656
MSFTserver <sawyerlemmo@gmail.com>
2017-11-01 11:36:19 -0700

    add more helping text to example

--- bot/modules/example.js
+++ bot/modules/example.js
@@ -1,22 +1,24 @@
 /*
-exports.commands = [
-	"demo" // command that is in this file, every command needs it own export as shown below
-]
+// Example #1 Command(use this to make your Commands with triggers like !demo)
+	exports.commands = [
+		"demo" // command name that will be used for next lines of code below
+	]
 
-exports.custom = [
-    "initFunction"
-]
+	exports.demo = {
+		usage: "<subcommand>", //command usage like !demo <@username>, exclude !demo
+		description: 'description of command', //the description of command for !help command
+		process: function(bot,msg,suffix){
+		// Here the bot,msg and suffix is avaible, this function can be async if needed.
+	    }
+	}
 
-exports.demo = {
-	usage: "<subcommand>",
-	description: 'description of command',
-	process: function(bot,msg,suffix){
-        // Here the bot,msg and suffix is avaible, this function can be async if needed.
-    }
-}
+// Example #2 Function(use this to make your Functions that dont need trigger words unlike !demo)
+	exports.custom = [
+	    "myFunction" //change this to your function name
+	]
 
-exports.initFunction = function(bot) {
-    // Other functions that needs to be ran once on bootup!
-    // For example a timed function and or some init stuff..
-}
+	exports.myFunction = function(bot) {
+	    // Other functions that needs to be ran once on bootup!
+	    // For example a timed function and or some init stuff..
+	}
 */
