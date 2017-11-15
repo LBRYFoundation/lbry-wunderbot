6a7b68afc34cf894272330dd815cc5029305a15d
MSFTserver <sawyerlemmo@gmail.com>
2017-11-15 12:46:43 -0800

    revamp

--- bot/modules/lbrylink.js
+++ bot/modules/lbrylink.js
@@ -1,13 +1,33 @@
+
 exports.custom = [
     "lbrylink" //change this to your function name
 ]
 
 exports.lbrylink = function(bot,msg,suffix) {
   bot.on('message', msg => {
-    if (msg.content.indexOf("lbry://") != -1) {
+    var link = msg.content.indexOf("lbry://")
+    if (link != -1) {
     var text = msg.content.replace("lbry://", "https://open.lbry.io/");
-    msg.delete(500);
-    msg.reply(" I see you tried to post a LBRY link let me help you with that:\n\n"+text)
+    var message = GetWordByPos(text, link)
+    var name = message.replace("https://open.lbry.io/", "");
+    const embed = {
+					"description": msg.author+", I see you tried to post a LBRY URL, here's a friendly hyperlink to share and for others to access your content with a single click: \n"+"[lbry://"+name+"]("+message+")",
+					"color": 7976557,
+					"author": {
+					     "name": "LBRY Linker",
+					     "icon_url": "https://i.imgur.com/yWf5USu.png"
+							}
+					};
+    msg.channel.send({embed})
   }
 })
+function GetWordByPos(str, pos) {
+    var left = str.substr(0, pos);
+    var right = str.substr(pos);
+
+    left = left.replace(/^.+ /g, "");
+    right = right.replace(/ .+$/g, "");
+
+    return left + right;
+}
 }
