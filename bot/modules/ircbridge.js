8f4cd19528f9be5a849f1914ae293ceb99c981fa
Fillerino <fillerix@fillerix.se>
2017-11-17 23:18:43 +0100

    Added irc-bridge, example in default.json.example
    Added irc-bridge, example in default.json.example

---
+++ bot/modules/ircbridge.js
@@ -0,0 +1,11 @@
+const discordIRC = require('elmadev-discord-irc').default;
+const config = require('config');
+const ircconfig = config.get('irc');
+	exports.custom = [
+	    "irc"
+	];
+
+	exports.irc = function(bot) {
+        discordIRC([ ircconfig ]);
+	};
+
