235eac753d4e261076982d2456c480fae2ab2e63
Fillerino <fillerix@fillerix.se>
2017-11-17 23:20:11 +0100

    Linting

--- bot/modules/ircbridge.js
+++ bot/modules/ircbridge.js
@@ -1,11 +1,8 @@
-const discordIRC = require('elmadev-discord-irc').default;
-const config = require('config');
-const ircconfig = config.get('irc');
-	exports.custom = [
-	    "irc"
-	];
+const discordIRC = require("elmadev-discord-irc").default;
+const config = require("config");
+const ircconfig = config.get("irc");
+exports.custom = ["irc"];
 
-	exports.irc = function(bot) {
-        discordIRC([ ircconfig ]);
-	};
-
+exports.irc = function(bot) {
+  discordIRC([ircconfig]);
+};
