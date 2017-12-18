26a53e77dee3564c4b50c3888d77cd5fb81a2e2f
MSFTserver <sawyerlemmo@gmail.com>
2017-12-18 15:49:39 -0800

    Revert "update lbry linker [DONE]"
    This reverts commit 11f4eb0d8be023f03892edccc54e583f5e7345ba.

--- bot/modules/lbrylink.js
+++ bot/modules/lbrylink.js
@@ -1,5 +1,5 @@
 let inPrivate = require("../helpers.js").inPrivate;
-let ResponseDebug = "false";
+
 exports.custom = [
   "lbrylink" //change this to your function name
 ];
@@ -13,31 +13,44 @@ exports.lbrylink = function(bot, msg, suffix) {
     if (link != -1) {
       var text = msg.content.replace("lbry://", "https://open.lbry.io/");
       var message = GetWordByPos(text, link);
-        if (ResponseDebug == "true") {
-          console.log("text = " + text);
-          console.log("message = " + message);
-        }
-      if (message === "https://open.lbry.io/") {
-        return;
-      }
-      if (message.search(">") != -1) {
-        parsename = message.split(">").pop();
-        if (parsename.search("/") == -1){
-          return;
-        }
-        newname = message.split("/").pop();
-        message = "https://open.lbry.io/" + newname;
-          if (ResponseDebug == "true") {
-            console.log("Username Provided!");
-            console.log("parsename = " + parsename);
-            console.log("newname = " + newname);
+      if (text.search("<") != -1) {
+        var name = "@" + msg.mentions.members.first().user.username;
+        var trim = message.split("/").pop();
+        var trim2 = trim.substr(2);
+        var id = trim2.substr(0, trim2.length - 1);
+        if (message.indexOf("#") != -1) {
+          if (trim.indexOf("@") != -1) {
+            var trim3 = message.split("#").pop();
+            var message = "https://open.lbry.io/" + name + "#" + trim3;
+            var newname = name + "#" + trim3;
+          } else {
+            var trim3 = message.split("/").pop();
+            var done = trim3;
+            var message = "https://open.lbry.io/" + name + "/" + done;
+            var newname = name + "/" + done;
           }
+        } else {
+          if (msg.mentions.members.first().id != id) {
+            var message =
+              "https://open.lbry.io/@" +
+              msg.mentions.members.first().user.username +
+              "/" +
+              message.split("/").pop();
+            var newname = name + "/" + message.split("/").pop();
+          } else {
+            var message =
+              "https://open.lbry.io/@" +
+              msg.mentions.members.first().user.username;
+            var newname = name;
+          }
+        }
       } else {
         var newname = message.replace("https://open.lbry.io/", "");
       }
       const embed = {
         description:
-          "I see you tried to post a LBRY URL, here's a friendly hyperlink to share and for others to access your content with a single click: \n" +
+          msg.author +
+          ", I see you tried to post a LBRY URL, here's a friendly hyperlink to share and for others to access your content with a single click: \n" +
           "[lbry://" +
           newname +
           "](" +
