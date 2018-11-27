c02e3647592a2f8117c995f07d3eeae0904f82f6
Niko Storni <niko@lbry.io>
2018-11-27 17:03:18 -0500

    rewrite claimbot
    add missing packages
    rename example config
    update example config

--- bot/modules/claimbot.js
+++ bot/modules/claimbot.js
@@ -1,26 +1,24 @@
 'use strict';
 
-let lbry;
 let mongo;
 let discordBot;
 let moment = require('moment');
-let request = require('request');
-let sleep = require('sleep');
 let config = require('config');
 let channels = config.get('claimbot').channels;
 const Discord = require('discord.js');
-const rp = require('request-promise');
-const jsonfile = require('jsonfile');
 const path = require('path');
 const fs = require('fs');
 const appRoot = require('app-root-path');
 const fileExists = require('file-exists');
+const request = require('request');
+let lastProcessedBlock = 0;
+
 module.exports = {
   init: init
 };
 
 function init(discordBot_) {
-  if (lbry) {
+  if (discordBot) {
     throw new Error('init was already called once');
   }
 
@@ -35,7 +33,7 @@ function init(discordBot_) {
       }
       mongo = db;
 
-      console.log('Activating claimbot ');
+      console.log('Activating claimbot');
       discordBot.channels.get(channels[0]).send('activating claimbot');
 
       // Check that our syncState file exist.
@@ -48,110 +46,101 @@ function init(discordBot_) {
         }
       });
       setInterval(function() {
-        //announceClaims(); pause the claim until https://github.com/lbryio/chainquery/issues/58 has been fixed.
+        announceClaimsV2();
       }, 60 * 1000);
       //announceClaims();
     }
-  );
+  ).catch(console.error);
 }
 
-async function announceClaims() {
-  // get last block form the explorer API.
-  let lastBlockHeight = JSON.parse(await rp('https://explorer.lbry.io/api/v1/status')).status.height;
-  // get the latest claims from chainquery since last sync
-  let syncState = await getJSON(path.join(appRoot.path, 'syncState.json')); // get our persisted state
-  if (!syncState.LastSyncTime) {
-    syncState.LastSyncTime = new Date()
-      .toISOString()
-      .slice(0, 19)
-      .replace('T', ' ');
-  }
-  let claimsSince = JSON.parse(await getClaimsSince(syncState.LastSyncTime)).data;
-  // filter out the claims that we should add to discord
-  let claims = [];
-  for (let claim of claimsSince) {
-    claim.value = JSON.parse(claim.value);
-    if (claim.value.Claim && claim.value.Claim.stream) {
-      claim.metadata = claim.value.Claim.stream.metadata;
-    } else {
-      claim.metadata = null;
-    }
-    if (claim.bid_state !== 'Spent' || claim.bid_state !== 'Expired') {
-      claims.push(claim);
-    }
-  }
-  for (let claim of claims) {
-    console.log(claim);
-  }
-  // send each claim to discord.
-  for (let claim of claims) {
-    console.log(claim);
-    if (claim.metadata) {
-      // If its a claim, make a claimEmbed
-      let claimEmbed = new Discord.RichEmbed()
-        .setAuthor(
-          claim.channel ? `New claim from ${claim.channel}` : 'New claim from Anonymous',
-          'http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/3dDoge.gif',
-          `http://open.lbry.io/${claim.channel ? `${claim.channel}#${claim.channelId}/${claim['name']}` : `${claim['name']}#${claim['claimId']}`}`
-        )
-        .setTitle('lbry://' + (claim.channel ? `${claim.channel}/` : '') + claim['name'])
-        .setURL(`http://open.lbry.io/${claim.channel ? `${claim.channel}#${claim.channelId}/${claim['name']}` : `${claim['name']}#${claim['claimId']}`}`)
+function announceClaimsV2() {
+  getClaimsForLastBlock()
+    .then(claims => {
+      let currentBlock = lastProcessedBlock;
+      claims.forEach(c => {
+        if (c.height <= lastProcessedBlock) return;
+        currentBlock = c.height;
+        console.log(JSON.stringify(c, null, 4));
+
+        //filter claims that we don't want to announce
+        if (c.bid_state === 'Expired' || c.bid_state === 'Spent') return;
+
+        discordPost(embedFromClaim(c));
+      });
+      lastProcessedBlock = currentBlock;
+    })
+    .catch(console.error);
+}
+
+/**
+ *
+ * @param {Object} claim
+ * @returns {RichEmbed} discordEmbeded
+ */
+function embedFromClaim(claim) {
+  let e = new Discord.RichEmbed();
+  const typeClaim = 1,
+    typeChannel = 2;
+  switch (claim.claim_type) {
+    case typeClaim:
+      let channelName = claim.channel_name ? claim.channel_name : 'Anonymous';
+      let channelPermalink = claim.channel_name ? `${claim.channel_name}#${claim.publisher_id}` : '';
+      let claimPermalink = claim.channel_name ? `${channelPermalink}/${claim.name}` : `${claim.name}#${claim.claim_id}`;
+      let metadata = JSON.parse(claim.value_as_json).claim.stream.metadata;
+      e.setAuthor(`New claim from ${channelName}`, 'http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/3dDoge.gif', `http://open.lbry.io/${claimPermalink}`)
+        .setTitle(`lbry://${claimPermalink}`)
+        .setURL(`http://open.lbry.io/${claimPermalink}`)
         .setColor(1399626)
-        .setFooter(`Block ${claim.height} • Claim ID ${claim.claimId} • Data from Chainquery`);
-      if (claim.metadata['title']) claimEmbed.addField('Title', claim.metadata['title']);
-      if (claim.channel) claimEmbed.addField('Channel', claim.channel);
-      if (claim.metadata['description']) {
-        claimEmbed.addField('Description', claim.metadata['description'].substring(0, 1020));
+        .setFooter(`Block ${claim.height} • Claim ID ${claim.claim_id} • Data from Chainquery`);
+      if (metadata.title) e.addField('Title', claim.metadata['title']);
+      if (claim.channel_name) e.addField('Channel', claim.channel_name);
+      if (metadata.description) {
+        e.addField('Description', claim.metadata.description.substring(0, 1020));
       }
-      if (claim.metadata['fee']) claimEmbed.addField('Fee', claim.metadata['fee'].amount + ' ' + claim.metadata['fee'].currency);
-      if (claim.metadata['license'] && claim.metadata['license'].length > 2) claimEmbed.addField('License', claim.metadata['license']);
-      if (!claim.metadata['nsfw'] && claim.metadata['thumbnail']) claimEmbed.setImage(claim.metadata['thumbnail']);
+      if (metadata.fee) e.addField('Fee', `${metadata.fee.amount} ${metadata.fee.currency}`);
+      if (metadata.license && metadata.license.length > 2) e.addField('License', metadata.license);
+      if (!metadata.nsfw && metadata.thumbnail) e.setImage(claim.metadata.thumbnail);
       if (claim.bid_state !== 'Controlling' && claim.height < claim.valid_at_height) {
         // Claim have not taken over the old claim, send approx time to event.
-        let takeoverTime = Date.now() + (claim.valid_at_height - lastBlockHeight) * 161 * 1000; // in theory this should be 150, but in practice its closer to 161
-        claimEmbed.addField('Takes effect on approx', moment(takeoverTime, 'x').format('MMMM Do [at] HH:mm [UTC]') + ` • at block height ${claim.valid_at_height}`);
+        let blockTime = 150 * 1000;
+        let takeoverTime = Date.now() + (claim.valid_at_height - lastBlockHeight) * blockTime;
+        e.addField('Takes effect on approx', `${moment(takeoverTime, 'x').format('MMMM Do [at] HH:mm [UTC]')} • at block height ${claim.valid_at_height}`);
       }
-      claimEmbed.addField('Claimed for', `${Number.parseFloat(claim.outputValue)} LBC`);
-      discordPost(claimEmbed);
-    } else if (claim.name.charAt(0) === '@') {
-      // This is a channel claim
-      let channelEmbed = new Discord.RichEmbed()
-        .setAuthor('New channel claim', 'http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/3dDoge.gif', `http://open.lbry.io/${claim['name']}#${claim['claimId']}`)
-        .setTitle('lbry://' + (claim.channel ? claim.channel + '/' : '') + claim['name'])
-        .setURL(`http://open.lbry.io/${claim['name']}#${claim['claimId']}`)
+      e.addField('Claimed for', `${Number.parseFloat(claim.bid_amount)} LBC`);
+      break;
+    case typeChannel:
+      e.setAuthor('New channel claim', 'http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/3dDoge.gif', `https://open.lbry.io/${claim.name}#${claim.claim_id}`)
+        .setTitle(`lbry://${claim.name}`)
+        .setURL(`https://open.lbry.io/${claim.name}#${claim.claim_id}`)
         .setColor(1399626)
-        .setFooter(`Block ${claim.height} • Claim ID ${claim.claimId} • Data from Chainquery`)
-        .addField('Channel Name', claim['name']);
-      discordPost(channelEmbed);
-    }
+        .setFooter(`Block ${claim.height} • Claim ID ${claim.claim_id} • Data from Chainquery`)
+        .addField('Channel Name', claim.name);
+      break;
   }
-  // set the last sync time to the db.
-  syncState.LastSyncTime = new Date()
-    .toISOString()
-    .slice(0, 19)
-    .replace('T', ' ');
-  await saveJSON(path.join(appRoot.path, 'syncState.json'), syncState);
+  return e;
 }
 
-function getJSON(path) {
+/**
+ *
+ * @returns {Promise} claims in last block
+ */
+function getClaimsForLastBlock() {
   return new Promise((resolve, reject) => {
-    jsonfile.readFile(path, function(err, jsoncontent) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(jsoncontent);
-      }
-    });
-  });
-}
-function saveJSON(path, obj) {
-  return new Promise((resolve, reject) => {
-    jsonfile.writeFile(path, obj, function(err, jsoncontent) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+    let query =
+      'SELECT t1.*, t3.name AS channel_name, t4.value AS bid_amount FROM claim t1 INNER JOIN (SELECT MAX(height) AS height FROM block) t2 ON t1.height = t2.height LEFT JOIN claim t3 ON t1.publisher_id = t3.claim_id LEFT JOIN output t4 ON (t1.transaction_hash_id = t4.transaction_hash AND t1.vout = t4.vout)';
+    let options = {
+      method: 'GET',
+      url: 'https://chainquery.lbry.io/api/sql',
+      qs: { query: query },
+      headers: { 'Cache-Control': 'no-cache' }
+    };
+
+    request(options, function(error, response, body) {
+      if (error) return reject(error);
+      if (response.statusCode !== 200 || !body) return reject(response);
+      if (!body.success || body.error) return reject(response);
+      let claimsInBlock = body.data;
+      return resolve(claimsInBlock);
     });
   });
 }
@@ -162,41 +151,5 @@ function discordPost(embed) {
       .get(channel)
       .send('', embed)
       .catch(console.error);
-  });
-}
-
-function getClaimsSince(time) {
-  return new Promise((resolve, reject) => {
-    let query =
-      `` +
-      `SELECT ` +
-      `c.name,` +
-      `c.valid_at_height,` +
-      `c.height,` +
-      `p.name as channel,` +
-      `c.publisher_id as channelId,` +
-      `c.bid_state,` +
-      `c.effective_amount,` +
-      `c.claim_id as claimId,` +
-      `c.value_as_json as value, ` +
-      `c.transaction_hash_id, ` + // txhash and vout needed to leverage old format for comparison.
-      `c.vout, ` +
-      `o.value as outputValue ` +
-      `FROM claim c ` +
-      `LEFT JOIN claim p on p.claim_id = c.publisher_id ` +
-      `LEFT JOIN output o on (o.transaction_hash=c.transaction_hash_id and o.vout=c.vout) ` +
-      `WHERE c.created_at >='` +
-      time +
-      `'`;
-    // Outputs full query to console for copy/paste into chainquery (debugging)
-    // console.log(query);
-    rp(`https://chainquery.lbry.io/api/sql?query=` + query)
-      .then(function(htmlString) {
-        resolve(htmlString);
-      })
-      .catch(function(err) {
-        console.log('error', '[Importer] Error getting updated claims. ' + err);
-        reject(err);
-      });
   });
 }
