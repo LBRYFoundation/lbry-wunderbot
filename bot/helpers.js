3b21e18b1655adfa9aa2e0d3db244cc97010363a
Ralph <blackbirdralph@aol.com>
2021-04-05 11:55:18 -0400

    Update member function for role check

--- bot/helpers.js
+++ bot/helpers.js
@@ -8,7 +8,7 @@ let statsBotChannels = config.get('statsbot');
 
 // Checks if user is allowed to use a command only for mods/team members
 exports.hasPerms = function(msg) {
-  return msg.member.roles !== null && msg.member.roles.some(r => permRanks.perms.includes(r.name));
+  return msg.member.roles !== null && msg.member.roles.cache.some(r => permRanks.perms.includes(r.name));
 };
 
 // Check if command was sent in dm
